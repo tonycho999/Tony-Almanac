4e422381ea01e0363cd1c27efa8e18fa080bd151
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-05-18 13:32:06 +0800

    Update and rename robots.txt to robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://tony-almanac.vercel.app/sitemap.xml',
+  }
+}
