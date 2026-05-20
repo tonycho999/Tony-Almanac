8ea6278e39ddc6891256090e5100da8aacf5e580
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-05-20 18:22:23 +0800

    Update route.ts

--- app/robots.txt/route.ts
+++ app/robots.txt/route.ts
@@ -1,7 +1,7 @@
 export async function GET() {
   const robots = `User-agent: *
 Allow: /
-Sitemap: https://tony-almanac.vercel.app/sitemap.xml`;
+Sitemap: https://tony-almanac.pages.dev/sitemap.xml`;
 
   return new Response(robots, {
     headers: { 'Content-Type': 'text/plain' },
