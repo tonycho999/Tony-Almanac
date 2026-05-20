8ae749217e5ea7dbcb2f1fe4c49725e2673a4162
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-05-20 15:32:41 +0800

    Delete next-sitemap.config.js

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://tony-almanac.vercel.app',
-  generateRobotsTxt: true,
-  // rss.xml도 정적 파일로 함께 생성하도록 설정 추가
-  additionalPaths: async (config) => [
-    await config.transform(config, '/rss.xml'),
-  ],
-};
