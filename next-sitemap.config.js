aa03891d52c4d13ccb7ec2c14a40c770ef2efec5
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-05-20 15:00:26 +0800

    Create next-sitemap.config.js

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://tony-almanac.vercel.app', // 본인 사이트 주소로 변경
+  generateRobotsTxt: true, // robots.txt 자동 생성
+  // sitemapSize: 7000, // 사이트맵이 클 경우 분할하는 옵션
+};
