78299a1a4eaeff305dbda3195e89e3b02b17f696
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-05-26 18:36:26 +0800

    Update and rename app/robots.txt/route.ts to app/robots.ts

--- app/robots.txt/route.ts
+++
@@ -1,9 +0,0 @@
-export async function GET() {
-  const robots = `User-agent: *
-Allow: /
-Sitemap: https://tony-almanac.pages.dev/sitemap.xml`;
-
-  return new Response(robots, {
-    headers: { 'Content-Type': 'text/plain' },
-  });
-}
